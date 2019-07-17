4d653ea4c826b52a13af75ee9f986b73cc57d4d3
keefertaylor <keefer.t@gmail.com>
2019-07-17 22:52:43 +0100

    Wire up index.js to actually run the program.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Automatic File Deduplication
 
 Do you feel like you've spent the last decade copying Time Machine Backups around? Are things getting out of hand? This handy deduplication tool might be the solution you are looking for.
+
+# Functionality
+
+This tool recursively traverses two directories and prints the diff. This tool uses a two tier comparison method to improve efficiency: first files are clustered by file size, then deduped within clusters using an md5 checksum.
+
+# Usage
+
+```
+$ node index.js <absolute path to dir1> <absolute path to dir2>
+```
+
--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-// TODO: Implement.
-console.log(process.argv)
+const argParser = require('./arg_parser.js')
+const fileSystemUtils = require('./file_system_utils')
+const deduplicationManager = require('./deduplicator_manager.js')
+
+const run = async function(dir1, dir2) {
+    console.log("Deduping across: ")
+    console.log("- " + dir1)
+    console.log("- " + dir2)
+
+    const dir1FileList = await fileSystemUtils.allFilesRecursively(args[0])
+    const dir2FileList = await fileSystemUtils.allFilesRecursively(args[1])
+    const filesToConsider = dir1FileList.concat(dir2FileList)
+
+    let duplicates = deduplicationManager.deduplicate(filesToConsider)
+
+    console.log(duplicates)
+}
+
+const args = argParser.validateArgs(process.argv)
+if (args == undefined) {
+    process.abort()
+}
+
+run(args[0], args[1])
