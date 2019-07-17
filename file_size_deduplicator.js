147e72a10280a48e0a85dbb6903b8d22f9e6f70e
keefertaylor <keefer.t@gmail.com>
2019-07-17 18:34:37 +0100

    Working tests

--- file_size_deduplicator.js
+++ file_size_deduplicator.js
@@ -3,34 +3,44 @@ const fs = require('fs')
 // Test cases:
 // - two same sized files
 // - two different sized files
-// - bad filenames
 
 const fileSizeDeduplicator = {
     /**
      * Deduplicate the given files by file size.
      * 
      * @param {Array<String>} filenames: A list of filenames to examine.
-     * @returns {Array<String>} A list of files that have the same size. Returns undefined if there was an error.
+     * @returns {Array<Array<String>>} A list of lists of potentially deduplicated files. Returns undefined if there was an error.
      */
     deduplicateByFileSize: function(filenames) {
-        var possibleDuplicates = []
-        var fileSizeMap = Map()
+        var fileSizeMap = {} //new Map()
 
-        for (filename in filenames) {
-            let size = fileSizeInBytes(filename)
+        for (var i = 0; i < filenames.length; i++) {
+            let filename = filenames[i];
+            let size = this.fileSizeInBytes(filename)
 
             // If the file couldn't be found, then bail out of the entire function since something has gone terribly wrong.
             if (size == undefined) {
                 return undefined;
             }
 
+            // Add the file to the list in the map.
             if (fileSizeMap[size] == undefined) {
-                fileSizeMap[size] = filename
+                fileSizeMap[size] = [filename]
             } else {
-                possibleDuplicates.push(filename)
+                fileSizeMap[size].push(filename)
             }
         }
-        return 
+
+        // Iterate over the list of files.
+        var potentiallyDuplicatedFiles = []
+        for (fileSize in fileSizeMap) {
+            let fileList = fileSizeMap[fileSize]
+            if (fileList.length != 1) {
+                potentiallyDuplicatedFiles.push(fileList)
+            }
+        }
+
+        return potentiallyDuplicatedFiles;
     },
 
     /**
--- test/file_size_deduplicator_tests.js
+++ test/file_size_deduplicator_tests.js
@@ -6,6 +6,11 @@ let test_data_dir = process.cwd() + '/test_data/'
 let testFile = test_data_dir + 'test_file.txt'
 let testFileSize = 10 // bytes
 
+let duplicatedFiles = [
+    test_data_dir + 'test_duplicated_file_1.txt',
+    test_data_dir + 'test_duplicated_file_2.txt'
+]
+
 let invalidFile = test_data_dir + 'DOES_NOT_EXIST.txt'
 
 describe('#fileSizeInBytes', function() {
@@ -19,3 +24,28 @@ describe('#fileSizeInBytes', function() {
         assert.equal(size, undefined);
     });
 });
+
+describe('#deduplicateByFileSize', function() {
+    it('should return undefined for a bad list of files', function() {
+        // GIVEN a list of files that has a non-existant file.
+        let filenames = duplicatedFiles.slice()
+        filenames.push(testFile, invalidFile)
+
+        // WHEN the files are deduplicated.
+        let potentialDuplicates = fileSizeDeduplicator.deduplicateByFileSize(filenames)
+
+        // THEN the result is undefined.
+        assert.equal(potentialDuplicates, undefined)
+    });
+
+    it('should return an empty list when there are no duplicated files', function() {
+        // GIVEN a list of files that are not duplicated.
+        let filenames = [ duplicatedFiles[0], testFile ]
+
+        // WHEN the files are deduplicated.
+        let potentialDuplicates = fileSizeDeduplicator.deduplicateByFileSize(filenames);
+
+        // THEN the result is undefined.
+        assert.equal(potentialDuplicates.length, 0);
+    });
+})
