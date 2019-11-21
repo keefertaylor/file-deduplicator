384a3fbb7f60814c327c677bed752a5dee388e1c
keefertaylor <keefer.t@gmail.com>
2019-11-20 20:47:06 -0500

    accept 1 arg

--- arg_parser.js
+++ arg_parser.js
@@ -3,10 +3,10 @@ const usageMessage = "Usage: `node index.js <absolute path to dir1> <absolute pa
 
 const argParser = {
     /**
-     * Validate the arguments and pass back an array of length 2 with the two directories. 
-     * 
+     * Validate the arguments and pass back an array of length 2 with the two directories.
+     *
      * Returns undefined if invalid args are provided.
-     * 
+     *
      * @param {Array<String>} argv The argv from the process.
      */
     validateArgs: function(argv) {
@@ -14,14 +14,24 @@ const argParser = {
         argv.shift()
         argv.shift()
 
-        if (argv.length != 2) {
+        if (argv.length < 1 || argv.length > 2) {
             console.log(usageMessage)
             return undefined
         }
 
         dir1 = argv.shift()
+        if (dir1.charAt(0) !== '/') {
+            console.log(absolutePathError)
+            console.log(usageMessage)
+            return undefined
+        }
+
         dir2 = argv.shift()
-        if (dir1.charAt(0) != '/' || dir2.charAt(0) != '/') {
+        if (dir2 === undefined) {
+          return [ dir1 ];
+        }
+
+        if (dir2.charAt(0) !== '/') {
             console.log(absolutePathError)
             console.log(usageMessage)
             return undefined
